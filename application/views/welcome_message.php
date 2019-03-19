9ba28d7ae831ba5e363d7ee79221896667518357
ictmp <doni.hardian@medikaplaza.com>
2019-03-19 16:59:59 +0700

    test 2

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?><!DOCTYPE html>
 <html lang="en">
-<head>
+ <head>
 	<meta charset="utf-8">
 	<title>Welcome to CodeIgniter</title>
 
